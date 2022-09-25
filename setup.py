774fa2d4c568691b83e7b6dca731cb780bd93e4c
Asit Piri <asit.piri@gmail.com>
2022-09-25 16:11:54 +0530

    setup config

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ import setuptools
 with open("README.md", "r", encoding = "utf-8") as f:
     long_description = f.read()
 
-__version__ = "0.0.0"       # update as per your need
+__version__ = "0.0.0"
 
-REPO_NAME = "<YOUR REPO NAME>"
-AUTHOR_USER_NAME = "<YOUR USERNAME>"
-SRC_REPO = "<YOUR REPO NAME>"
-AUTHOR_EMAIL = "<YOUR EMAIL ID>"
+REPO_NAME = "IPYNBrenderer"
+AUTHOR_USER_NAME = "asit-pir"
+SRC_REPO = "IPYNBrenderer"
+AUTHOR_EMAIL = "asit.piri@gmail.com"
 
 setuptools.setup(
     name = SRC_REPO,
